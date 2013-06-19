c46dcad0e76fcb69ff7fe70c0720d815b3564f9f
Antoine BERNIER <antoine.bernier@gmail.com>
2013-06-19 12:04:06 +0200

    default couch

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var rc = require('rc');
 
 module.exports = rc('rum', {
   aum_username: 'john@example.org',
-  aum_password: '******'
+  aum_password: '******',
+  couch: 'http://localhost:5984/rum'
 });
